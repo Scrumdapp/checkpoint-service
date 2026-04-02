a4924c7986abeec7590004fd2c21c0092251a23b
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-04-02 09:30:44 +0200

    removed some unused files

--- src/main/kotlin/com/scrumdapp/checkpointservice/configs/SecurityConfiguration.kt
+++ src/main/kotlin/com/scrumdapp/checkpointservice/configs/SecurityConfiguration.kt
@@ -18,7 +18,6 @@ public class SecurityConfiguration: WebMvcConfigurer {
             .csrf { it.disable() }
             .authorizeHttpRequests(this::createRequestMatcher)
 
-
         return httpSec.build()
     }
 
