f6a07184861fcff433d4f33c820fd2b687a1ce5f
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-04-04 14:08:54 +0200

    Small fix to whitelabel errorhandler

--- src/main/kotlin/com/scrumdapp/checkpointservice/controllers/ExceptionController.kt
+++ src/main/kotlin/com/scrumdapp/checkpointservice/controllers/ExceptionController.kt
@@ -1,18 +1,27 @@
 package com.scrumdapp.checkpointservice.controllers
 
 import com.scrumdapp.checkpointservice.ApiException
+import com.scrumdapp.checkpointservice.NotFoundException
 import jakarta.servlet.RequestDispatcher
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.boot.autoconfigure.web.ErrorProperties
+import org.springframework.boot.webmvc.autoconfigure.error.BasicErrorController
+import org.springframework.boot.webmvc.error.ErrorAttributes
+import org.springframework.boot.webmvc.error.ErrorController
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ErrorController {
+class ExceptionController: ErrorController {
 
     @GetMapping("/error")
     fun handleError(req: HttpServletRequest) {
         val status = Integer.valueOf(req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString())
         val message = req.getAttribute(RequestDispatcher.ERROR_MESSAGE).toString()
-        throw ApiException(status, message)
+
+        when (status) {
+            404 -> throw NotFoundException()
+            else -> throw ApiException(status, message)
+        }
     }
 }
