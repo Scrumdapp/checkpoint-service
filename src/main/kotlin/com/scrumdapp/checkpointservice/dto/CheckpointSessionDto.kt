14a50353e9708cf01abf107673353e857dcc06da
HahaGh0sty <181955075+HahaGh0sty@users.noreply.github.com>
2026-06-05 12:21:21 +0200

    got rid of unused imports

--- src/main/kotlin/com/scrumdapp/checkpointservice/dto/CheckpointSessionDto.kt
+++ src/main/kotlin/com/scrumdapp/checkpointservice/dto/CheckpointSessionDto.kt
@@ -1,12 +1,11 @@
 package com.scrumdapp.checkpointservice.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
+
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
-import java.time.LocalDate
-import java.time.LocalTime
+
 
 sealed interface SessionResponseDto {
     val id: Long
