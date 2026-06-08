68f0b306a4f3188f66e139e84b4a3af95e46d37b
TheLonelyKiwi <gamingkiwi22@gmail.com>
2026-06-08 13:47:33 +0200

    cleanup error handling

--- src/main/kotlin/com/scrumdapp/checkpointservice/errors/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/scrumdapp/checkpointservice/errors/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.scrumdapp.checkpointservice.errors
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 @Component
 class GlobalExceptionHandler {
+    val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
     @ExceptionHandler(ApiException::class)
     fun handleApiException(e: ApiException): ResponseEntity<ApiExceptionResponse> {
+        if (e.enableLogging) logger.error(e.message, e.stackTrace)
         return ResponseEntity.status(e.code).body(ApiExceptionResponse(e.code, e.message))
     }
 
@@ -20,6 +23,12 @@ class GlobalExceptionHandler {
         val message = e.bindingResult.fieldErrors
             .joinToString(", ") { it.defaultMessage ?: "Invalid field" }
         return ResponseEntity.status(400).body(ApiExceptionResponse(400, message))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<ApiExceptionResponse> {
+        logger.error(e.message, e.stackTrace)
+        return ResponseEntity.status(500).body(ApiExceptionResponse(500, "Something went wrong"))
     }
 }
 
--- src/main/kotlin/com/scrumdapp/checkpointservice/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/scrumdapp/checkpointservice/security/SecurityConfiguration.kt
@@ -1,27 +1,19 @@
 package com.scrumdapp.checkpointservice.security
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.scrumdapp.checkpointservice.errors.ApiExceptionResponse
+import com.scrumdapp.checkpointservice.errors.CustomAccessDeniedHandler
+import com.scrumdapp.checkpointservice.errors.CustomAuthEntrypointHandler
 import com.scrumdapp.passportplugin.filters.PassportAuthFilter
 import com.scrumdapp.passportplugin.filters.usePassport
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.AuthenticationException
-import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration(
-    private val customAuthEntryPoint: CustomAuthEntryPoint,
     private val passportAuthFilter: PassportAuthFilter
 ): WebMvcConfigurer {
 
@@ -35,34 +27,12 @@ public class SecurityConfiguration(
                 it.requestMatchers("/groups/{groupId}/checkpoints/**").hasAnyAuthority("STUDENT", "COACH")
                 it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
             }
-            .exceptionHandling {
-                it.authenticationEntryPoint(customAuthEntryPoint)
-                it.accessDeniedHandler { _, response, _ ->
-                    response.status = HttpServletResponse.SC_FORBIDDEN
-                }
+            .exceptionHandling { ex -> ex
+                .authenticationEntryPoint(CustomAuthEntrypointHandler())
+                .accessDeniedHandler(CustomAccessDeniedHandler())
             }
 
         return http.build()
-    }
-}
-
-
-
-
-// Move this to another file
-@Component
-class CustomAuthEntryPoint: AuthenticationEntryPoint {
-    override fun commence(
-        request: HttpServletRequest,
-        response: HttpServletResponse,
-        authException: AuthenticationException
-    ) {
-        if (response.isCommitted) return
-        response.status = HttpServletResponse.SC_UNAUTHORIZED
-        response.contentType = MediaType.APPLICATION_JSON_VALUE
-        ObjectMapper().writeValue(response.writer,
-            ApiExceptionResponse(HttpStatus.UNAUTHORIZED.value(), authException.message)
-        )
     }
 }
 
